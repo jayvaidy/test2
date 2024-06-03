907e2186edd869471a80452aeb470090fcca8b8e
jayvaidy <57367874+jayvaidy@users.noreply.github.com>
2024-06-03 15:53:22 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,8 +15,8 @@ In the meantime, below is an example of what you can do with just a few lines of
 toggle = st.toggle("Turn on and off the following text box", False)
 if(toggle):
     st.text_area("Test input here:", max_chars=1000, key="input1", placeholder="Placeholder Text...", disabled=toggle)
-num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
-num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
+    num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
+    num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
 
 indices = np.linspace(0, 1, num_points)
 theta = 2 * np.pi * num_turns * indices
