37cef31b9c68fe9c0ba33e5748019c0c24736108
jayvaidy <57367874+jayvaidy@users.noreply.github.com>
2024-06-03 16:01:10 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,17 +3,11 @@ import numpy as np
 import pandas as pd
 import streamlit as st
 
-"""
-# Welcome to Streamlit!
-
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
 toggle = st.toggle("Turn on and off the following text box", False)
-test_input = None
+first_run = true
+if(first_run):
+    test_input = None
+    first_run = false
 if(toggle):
     test_input = st.text_area("Test input here:", max_chars=1000, key="input1", placeholder="Placeholder Text...")
 num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
