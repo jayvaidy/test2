c37ad955505974d0823aefa49634d8108cd91524
jayvaidy <57367874+jayvaidy@users.noreply.github.com>
2024-06-03 15:45:39 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,8 +12,8 @@ forums](https://discuss.streamlit.io).
 
 In the meantime, below is an example of what you can do with just a few lines of code:
 """
-
-st.text_area("test_label", None, None, 1000)
+toggle = st.toggle("Turn on and off the following text box", False, )
+st.text_area("Test input here:", None, None, 1000, "input1", None, None, None, None, "Placeholder Text...", toggle)
 
 num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
 num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
