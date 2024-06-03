5745c82b6a2f6ef09affa2095aa322e57181a848
jayvaidy <57367874+jayvaidy@users.noreply.github.com>
2024-06-03 15:57:29 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,7 @@ if(toggle):
     test_input = st.text_area("Test input here:", max_chars=1000, key="input1", placeholder="Placeholder Text...")
 num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
 num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
-if(test_input != None)
+if(test_input != None):
     st.write(test_input)
 
 indices = np.linspace(0, 1, num_points)
